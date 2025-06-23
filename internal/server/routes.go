1206db37eb3d92710be32c6e8bad11c012a3d199
Joel Sequeira <joelseq96@gmail.com>
2025-06-22 20:39:19 -0400

    Change CORS config

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -18,13 +18,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	e.Use(prettylogger.Logger)
 	e.Use(middleware.Recover())
 
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"https://*", "http://*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
+	e.Use(middleware.CORS())
 
 	// SQLite admin setup
 	config := sqliteadmin.Config{
